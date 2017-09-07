dc86ee934134f6c3790f9d7ea94d2189415ac421
Gus Dunn <w.gus.dunn@gmail.com>
2017-09-07 16:02:48 -0400

    tests more robust

--- tests/test_easygv.py
+++ tests/test_easygv.py
@@ -2,13 +2,24 @@
 # -*- coding: utf-8 -*-
 
 """Tests for `easygv` package."""
+import re
 
 import pytest
 
 from click.testing import CliRunner
 
-from easygv import easygv
+import pandas as pd
+
 from easygv import cli
+from easygv import easygv
+
+
+def is_in(rgx, string):
+    """Return True if the regex is found in the string."""
+    if rgx.search(string):
+        return True
+    else:
+        return False
 
 
 @pytest.fixture
@@ -30,9 +41,25 @@ def test_content(response):
 def test_command_line_interface():
     """Test the CLI."""
     runner = CliRunner()
+    help_option_line = re.compile("--help\W+Show this message and exit.")
+
     result = runner.invoke(cli.main)
     assert result.exit_code == 0
-    assert 'easygv.cli.main' in result.output
+    assert 'main' in result.output
     help_result = runner.invoke(cli.main, ['--help'])
     assert help_result.exit_code == 0
-    assert '--help  Show this message and exit.' in help_result.output
+    assert is_in(rgx=help_option_line, string=help_result.output)
+
+
+def test_nan_to_str():
+    """Test conversion of NaN values to string."""
+    nan = pd.np.nan
+    str_ = "testing"
+    int_ = 12
+    float_ = 67.9
+
+    assert easygv.nan_to_str(x=nan) == ""
+    assert easygv.nan_to_str(x=str_) is str_
+    assert easygv.nan_to_str(x=int_) is int_
+    assert easygv.nan_to_str(x=float_) is float_
+    assert easygv.nan_to_str(x=pd) is pd
